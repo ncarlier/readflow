59c0ade36f67cf2d5644c285296b16e3db1db937
Nicolas Carlier <n.carlier@nunux.org>
2019-03-18 11:23:45 +0000

    chore(): add autogen dir

---
+++ autogen/db/postgres/db_sql_migration.go
@@ -0,0 +1,91 @@
+// Code generated. DO NOT EDIT!
+
+package postgres
+
+var DatabaseSQLMigration = map[string]string{
+	"db_migration_1": `create table schema_version (
+  version text not null
+);
+
+
+create table users (
+  id serial not null,
+  username text not null unique,
+  enabled bool not null default 't',
+  last_login_at timestamp with time zone,
+  created_at timestamp with time zone not null default now(),
+  updated_at timestamp with time zone null,
+
+  primary key (id)
+);
+
+create table categories (
+  id serial not null,
+  user_id int not null,
+  title text not null,
+  created_at timestamp with time zone not null default now(),
+  updated_at timestamp with time zone null,
+
+  primary key (id),
+  unique (user_id, title),
+  foreign key (user_id) references users(id) on delete cascade
+);
+
+create type article_status as enum('unread', 'read');
+
+create table articles (
+  id bigserial not null,
+  user_id int not null,
+  category_id int null,
+  title text not null,
+  text text,
+  html text,
+  url text,
+  image text,
+  hash text not null,
+  status article_status default 'unread',
+  published_at timestamp with time zone null,
+  created_at timestamp with time zone not null default now(),
+  updated_at timestamp with time zone null,
+    
+  primary key (id),
+  unique (user_id, hash),
+  foreign key (user_id) references users(id) on delete cascade,
+  foreign key (category_id) references categories(id) on delete set null
+);
+
+create index articles_user_status_idx on articles(user_id, status);
+create index articles_user_status_category_idx on articles(user_id, status, category_id);
+
+create table api_keys (
+  id serial not null,
+  user_id int not null,
+  alias text not null,
+  token text not null unique,
+  last_usage_at timestamp with time zone,
+  created_at timestamp with time zone not null default now(),
+  updated_at timestamp with time zone null,
+
+  primary key (id),
+  unique (user_id, alias),
+  foreign key (user_id) references users(id) on delete cascade
+);
+
+create table rules (
+  id serial not null,
+  user_id int not null,
+  priority int not null,
+  rule text not null,
+  action text not null,
+  created_at timestamp with time zone not null default now(),
+  updated_at timestamp with time zone null,
+
+  primary key (id),
+  foreign key (user_id) references users(id) on delete cascade
+)
+`,
+}
+
+var DatabaseSQLMigrationChecksums = map[string]string{
+	"db_migration_1": "1bca823b5202b58b197f8409e5b60f4b5724c094e50c6c9ad6d51ca1b82207f6",
+}
