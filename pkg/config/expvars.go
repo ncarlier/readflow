29070c0e2a88dc35b8c85d9c6039daf028134d17
Nicolas Carlier <n.carlier@nunux.org>
2019-04-29 16:07:29 +0000

    feat(): add configuration variables to expvars

---
+++ pkg/config/expvars.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"expvar"
+	"time"
+)
+
+var conf = expvar.NewMap("config")
+
+func getConfString(val *string) func() interface{} {
+	return func() interface{} {
+		if val == nil {
+			return nil
+		}
+		return *val
+	}
+}
+
+func getConfDur(val *time.Duration) func() interface{} {
+	return func() interface{} {
+		if val == nil {
+			return nil
+		}
+		return val.String()
+	}
+}
+
+func init() {
+	conf.Set("addr", expvar.Func(getConfString(config.ListenAddr)))
+	conf.Set("authn", expvar.Func(getConfString(config.AuthN)))
+	conf.Set("public-url", expvar.Func(getConfString(config.PublicURL)))
+}
