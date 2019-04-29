fef557ea2dcf1b9c1d17c2ea84ab0d4d51393df9
Nicolas Carlier <n.carlier@nunux.org>
2019-04-29 17:12:39 +0000

    fix(): lint public js files

--- ui/public/custom-sw.js
+++ ui/public/custom-sw.js
@@ -1,6 +1,6 @@
-
+/* eslint-disable no-undef */
 self.addEventListener('push', function(event) {
-  if (!("Notification" in self && Notification.permission === 'granted')) {
+  if (!('Notification' in self && Notification.permission === 'granted')) {
     return
   }
 
@@ -9,35 +9,33 @@ self.addEventListener('push', function(event) {
     try {
       data = event.data.json()
     } catch (err) {
-      data = {body: event.data.text()}
+      data = { body: event.data.text() }
     }
   }
-  let title = data.title || "Something has happened"
+  let title = data.title || 'Something has happened'
   let body = data.body || "Here's something you might want to check out."
-  const icon = "logo.png"
-  const tag = "readflow-notification"
+  const icon = 'logo.png'
+  const tag = 'readflow-notification'
 
-  event.waitUntil(
-    self.registration.showNotification(title, {body, icon, tag})
-  )
+  event.waitUntil(self.registration.showNotification(title, { body, icon, tag }))
 })
 
 self.addEventListener('notificationclick', function(event) {
   console.log('On notification click: ', event.notification.tag)
   event.notification.close()
   event.waitUntil(
-    clients.matchAll({
-      type: "window"
-    })
-    .then(function(clientList) {
-      for (var i = 0; i < clientList.length; i++) {
-        var client = clientList[i];
-        if (client.url == '/' && 'focus' in client)
-          return client.focus()
-      }
-      if (clients.openWindow) {
-        return clients.openWindow('/')
-      }
-    })
+    clients
+      .matchAll({
+        type: 'window'
+      })
+      .then(function(clientList) {
+        for (var i = 0; i < clientList.length; i++) {
+          var client = clientList[i]
+          if (client.url == '/' && 'focus' in client) return client.focus()
+        }
+        if (clients.openWindow) {
+          return clients.openWindow('/')
+        }
+      })
   )
 })
