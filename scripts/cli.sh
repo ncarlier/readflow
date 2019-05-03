f0263965d13f8300efce965ade3cbc26ccbd86af
Nicolas Carlier <n.carlier@nunux.org>
2019-05-03 12:21:54 +0000

    chore(): add CLI script

---
+++ scripts/cli.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source .env
+
+authority=https://login.nunux.org/auth/realms/readflow
+endpoint=https://api.readflow.app
+
+payload="grant_type=client_credentials&client_id=${client_id}&client_secret=${client_secret}"
+
+
+res=`curl -s -k --data "${payload}" ${authority}/protocol/openid-connect/token`
+access_token=`echo $res | jq -r .access_token`
+
+curl -s -H "Authorization: Bearer $access_token" ${endpoint}$1
