752eddd4abb16b2bcc947f9e6efbe185da227817
Nicolas Carlier <n.carlier@nunux.org>
2020-01-15 19:25:21 +0000

    feat(): send notification only if user is inactive

--- pkg/event-listener/article-notification.go
+++ pkg/event-listener/article-notification.go
@@ -5,12 +5,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"math"
+	"time"
 
 	"github.com/ncarlier/readflow/pkg/constant"
-
 	"github.com/ncarlier/readflow/pkg/event"
 	"github.com/ncarlier/readflow/pkg/model"
 	"github.com/ncarlier/readflow/pkg/service"
+
+	"github.com/rs/zerolog/log"
 )
 
 func init() {
@@ -21,15 +23,30 @@ func init() {
 			ctx := context.WithValue(context.TODO(), constant.UserID, uid)
 			req := model.ArticlesPageRequest{Status: &status}
 
-			nb, err := service.Lookup().CountArticles(ctx, req)
+			user, err := service.Lookup().GetCurrentUser(ctx)
 			if err != nil {
-				return
-			}
-			if !(nb > 0 && math.Mod(float64(nb), 10) == 0) {
-				// No need to notify the user
+				log.Info().Err(err).Uint("id", uid).Msg("unable to send notification")
 				return
 			}
 
+			// Send notification only if user logged in more than 5 minutes ago
+			lastLoginDelay := time.Now().Add(-5 * time.Minute)
+			if user.Enabled && user.LastLoginAt != nil && user.LastLoginAt.After(lastLoginDelay) {
+				return
+			}
+
+			nb, err := service.Lookup().CountArticles(ctx, req)
+			if err != nil {
+				log.Info().Err(err).Uint("id", uid).Msg("unable to send notification")
+				return
+			}
+
+			// Send notification only every 10 articles
+			if !(nb > 0 && math.Mod(float64(nb), 10) == 0) {
+				return
+			}
+
+			// Build notification
 			text := fmt.Sprintf("You have %d articles to read.", nb)
 			notif := &model.DeviceNotification{
 				Title: "New articles to read",
@@ -40,7 +57,7 @@ func init() {
 				text = string(b)
 			}
 
-			// User notification
+			// Notify all user devices
 			service.Lookup().NotifyDevices(ctx, text)
 		}
 	})
