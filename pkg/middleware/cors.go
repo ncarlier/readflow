816aff73859a05785f23ee441093c38bc02d04e5
Nicolas Carlier <n.carlier@nunux.org>
2020-03-19 20:05:45 +0000

    feat(api): allows localhost for CORS

--- pkg/middleware/cors.go
+++ pkg/middleware/cors.go
@@ -2,13 +2,21 @@ package middleware
 
 import (
 	"net/http"
+	"net/url"
 )
 
 // Cors is a middleware to enabling CORS on HTTP requests
 func Cors(allowOrigin string) Middleware {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Access-Control-Allow-Origin", allowOrigin)
+			origin := r.Header.Get("Origin")
+			u, err := url.Parse(origin)
+			if !(err == nil && u.Hostname() == "localhost") {
+				// Use configured origin for non localhost requests
+				origin = allowOrigin
+			}
+
+			w.Header().Set("Access-Control-Allow-Origin", origin)
 			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization")
 
