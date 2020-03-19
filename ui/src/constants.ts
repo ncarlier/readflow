db2eff834971507f5bf6376224440f5813ea6385
Nicolas Carlier <n.carlier@nunux.org>
2020-03-19 14:31:42 +0000

    chore(): switch to login.readflow.app

--- ui/src/constants.ts
+++ ui/src/constants.ts
@@ -2,7 +2,7 @@
 export const API_BASE_URL = process.env.REACT_APP_API_ROOT || 'https://api.readflow.app'
 
 // OIDC authority URL
-export const AUTHORITY = process.env.REACT_APP_AUTHORITY || 'https://login.nunux.org/auth/realms/readflow'
+export const AUTHORITY = process.env.REACT_APP_AUTHORITY || 'https://login.readflow.app/auth/realms/readflow'
 
 // OIDC client ID
 export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID || 'webapp'
