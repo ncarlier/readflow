65f7f2f772f3641c5296ebbd2f872ae3bc10f7e6
Nicolas Carlier <n.carlier@nunux.org>
2020-08-16 07:42:50 +0000

    feat(ui): add lazy image component

---
+++ ui/src/components/LazyImage.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useRef, useEffect } from 'react'
+
+import { API_BASE_URL } from '../constants'
+import { useMedia } from '../hooks'
+import { classNames } from '../helpers'
+
+import styles from './LazyImage.module.css'
+
+interface Props {
+  src: string
+  alt?: string
+}
+
+const proxifyImageURL = (url: string, width: number) =>
+  `${API_BASE_URL}/img?url=${encodeURIComponent(url)}&width=${width}`
+
+export default ({ src, alt = '' }: Props) => {
+  const mobileDisplay = useMedia('(max-width: 767px)')
+  const [loaded, setLoaded] = useState(false)
+  const imgRef = useRef<HTMLImageElement>(null)
+  useEffect(() => {
+    if (imgRef.current && imgRef.current.complete) {
+      setLoaded(true)
+    }
+  }, [])
+
+  return (
+    <div className={styles.wrapper}>
+      <img src={proxifyImageURL(src, 64)} aria-hidden="true" alt={alt} className={styles.lqip} />
+      <img
+        ref={imgRef}
+        src={src}
+        srcSet={`${proxifyImageURL(src, 320)} 320w,
+                ${proxifyImageURL(src, 767)} 767w`}
+        sizes={mobileDisplay ? '100vw' : '320px'}
+        alt={alt}
+        loading="lazy"
+        className={classNames(styles.source, loaded ? styles.loaded : null)}
+        onLoad={() => setLoaded(true)}
+        onError={(e) => (e.currentTarget.style.display = 'none')}
+      />
+    </div>
+  )
+}
