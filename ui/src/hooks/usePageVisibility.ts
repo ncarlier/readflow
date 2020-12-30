d305543a07196084983ed64a8d110f3ceb2b6aaa
Nicolas Carlier <n.carlier@nunux.org>
2020-12-30 09:39:27 +0000

    feat(ui): improve visibility API usage

--- ui/src/hooks/usePageVisibility.ts
+++ ui/src/hooks/usePageVisibility.ts
@@ -1,38 +1,20 @@
 import { useEffect, useState } from 'react'
 
-const getPageVisibilityApiAttributes = () => {
-  let hidden = 'hidden'
-  let visibilityChange = 'visibilitychange'
-  if ('mozHidden' in document) {
-    // Firefox up to v17
-    hidden = 'mozHidden'
-    visibilityChange = 'mozvisibilitychange'
-  } else if ('webkitHidden' in document) {
-    // Chrome up to v32, Android up to v4.4, Blackberry up to v10
-    hidden = 'webkitHidden'
-    visibilityChange = 'webkitvisibilitychange'
-  }
-  return { hidden, visibilityChange }
-}
-
 const isSupported = () => {
-  const { hidden } = getPageVisibilityApiAttributes()
-  return hidden in document
+  return 'visibilityState' in document
 }
-
-const { hidden, visibilityChange } = getPageVisibilityApiAttributes()
 
 export default () => {
-  const [isVisible, setIsVisible] = useState<boolean>(!isSupported() || (document as any)[hidden])
+  const [isVisible, setIsVisible] = useState<boolean>(!isSupported() || document.visibilityState === 'visible')
 
   useEffect(() => {
     if (isSupported()) {
       const handler = () => {
-        setIsVisible(!(document as any)[hidden])
+        setIsVisible(document.visibilityState === 'visible')
       }
-      window.addEventListener(visibilityChange, handler)
+      window.addEventListener('visibilitychange', handler)
       return () => {
-        window.removeEventListener(visibilityChange, handler)
+        window.removeEventListener('visibilitychange', handler)
       }
     }
   }, [])
