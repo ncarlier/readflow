ea4df78ecabe2ab532302dfcd5a191d8a9e0a2b6
Nicolas Carlier <n.carlier@nunux.org>
2021-05-12 21:33:50 +0000

    chore(landing): hide WIP elements

--- landing/components/Wip.jsx
+++ landing/components/Wip.jsx
@@ -1,5 +1,5 @@
 const isDev = process.env.NODE_ENV === 'development'
 
-const Wip = ({ children }) => !isDev ? <>{ children }</> : null
+const Wip = ({ children }) => isDev ? <>{ children }</> : null
 
 export default Wip
