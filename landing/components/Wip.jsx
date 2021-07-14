8d19c6432aaae4e701b0eef47200daaae4230c8f
Nicolas Carlier <n.carlier@nunux.org>
2021-07-14 09:14:34 +0000

    feat(landing): fix WIP status

--- landing/components/Wip.jsx
+++ landing/components/Wip.jsx
@@ -1,5 +1,5 @@
 const isDev = process.env.NODE_ENV === 'development'
 
-const Wip = ({children, placeholder}) => !isDev ? <>{ children }</> : <>{ placeholder }</>
+const Wip = ({children, placeholder}) => isDev ? <>{ children }</> : <>{ placeholder }</>
 
 export default Wip
