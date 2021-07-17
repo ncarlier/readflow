de989743f2f906c496d2057574e8796d1ce09da1
Nicolas Carlier <n.carlier@nunux.org>
2021-07-17 13:38:35 +0000

    fix(ui): fix invalid_grant exception

--- ui/src/graphqlClient.ts
+++ ui/src/graphqlClient.ts
@@ -33,8 +33,9 @@ const authenticationLink = setContext(async (_, { headers }) => {
 const errorLink = onError((err) => {
   console.error(err)
   if (err.networkError) {
-    console.log('networkError:', err.networkError.name, err.networkError.message)
-    if (err.networkError.message === 'login_required') {
+    const { name, message } = err.networkError
+    console.log('networkError:', name, message)
+    if (message === 'login_required' || message === 'invalid_grant') {
       authService.login()
     }
   }
