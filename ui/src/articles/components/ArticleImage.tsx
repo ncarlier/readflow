31136f9711d476a9703cdee46cf4db75db15d9e0
Nicolas Carlier <nicolas.carlier@worldline.com>
2022-03-22 08:55:54 +0100

    fix(ui): fix responsive image rendering

--- ui/src/articles/components/ArticleImage.tsx
+++ ui/src/articles/components/ArticleImage.tsx
@@ -1,28 +1,19 @@
-import React from 'react'
+import React, { FC, ImgHTMLAttributes } from 'react'
 
 import { API_BASE_URL } from '../../constants'
-import { useMedia } from '../../hooks'
-
-interface Props {
-  src: string
-  alt?: string
-}
 
 const proxifyImageURL = (url: string, width: number) =>
   `${API_BASE_URL}/img?url=${encodeURIComponent(url)}&width=${width}`
 
-export const ArticleImage = ({ src, alt = '' }: Props) => {
-  const mobileDisplay = useMedia('(max-width: 767px)')
-  const attrs: any = {}
-  if (src.match(/^https?:\/\//)) {
+export const ArticleImage: FC<ImgHTMLAttributes<HTMLImageElement>> = ({ src, ...attrs }) => {
+  if (src && src.match(/^https?:\/\//)) {
     attrs.srcSet = `${proxifyImageURL(src, 320)} 320w, ${proxifyImageURL(src, 767)} 767w`
   }
   return (
     <img
-      src={src}
       {...attrs}
-      sizes={mobileDisplay ? '100vw' : '320px'}
-      alt={alt}
+      sizes="(max-width: 767px) 767px, 320px"
+      src={src}
       onError={(e) => (e.currentTarget.style.display = 'none')}
     />
   )
