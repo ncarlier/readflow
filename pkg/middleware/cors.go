9bf0dc276050cddb3773790b96620f1449b1f566
Nicolas Carlier <n.carlier@nunux.org>
2023-05-01 13:04:10 +0000

    chore(): use CORS library

--- pkg/middleware/cors.go
+++ pkg/middleware/cors.go
@@ -1,29 +1,14 @@
 package middleware
 
 import (
-	"net/http"
-	"net/url"
+	"github.com/rs/cors"
 )
 
 // Cors is a middleware to enabling CORS on HTTP requests
 func Cors(allowOrigin string) Middleware {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			origin := r.Header.Get("Origin")
-			u, err := url.Parse(origin)
-			if !(err == nil && u.Hostname() == "localhost") {
-				// Use configured origin for non localhost requests
-				origin = allowOrigin
-			}
-
-			w.Header().Set("Access-Control-Allow-Origin", origin)
-			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
-			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization")
-			w.Header().Set("Access-Control-Expose-Headers", "*")
-
-			if r.Method != "OPTIONS" {
-				next.ServeHTTP(w, r)
-			}
-		})
-	}
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{allowOrigin},
+		AllowCredentials: true,
+	})
+	return c.Handler
 }
