781b6d320d5fdbb33d8806381047408443e19a24
Nicolas Carlier <n.carlier@nunux.org>
2024-04-20 06:54:38 +0000

    chore(ui): add meta description header

--- landing/components/Layout.jsx
+++ landing/components/Layout.jsx
@@ -27,15 +27,17 @@ const AutheticatedPage = ({children}) => (
 const Layout = ({
   children,
   title = 'Welcome to readflow!',
+  desc = 'readflow is a news-reading (or read-it-later) solution focused on versatility and simplicity',
   authenticated = false,
 }) => (
   <>
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
+      <meta name="description" content={desc} />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta property="og:title" content="readflow" />
-      <meta property="og:description" content="Read your Internet article flow in one place with complete peace of mind and freedom" />
+      <meta property="og:description" content="read your Internet article flow in one place with complete peace of mind and freedom" />
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://about.readflow.app" />
       <meta property="og:image" content="https://about.readflow.app/images/readflow.png" />
